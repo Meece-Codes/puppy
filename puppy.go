b0d7e4f04f4740fa2757923dd48c0c33f1ed0603
Meece <meece.code@gmail.com>
2024-01-20 19:27:11 +0200

    Add version function

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/Meece-Codes/dog"
 )
 
@@ -18,4 +20,8 @@ func Barking() string {
 
 func BigBark() string {
 	return dog.WhenGrownUp(Bark())
+}
+
+func from13() {
+	fmt.Println("I'm from version 1.1.0")
 }
